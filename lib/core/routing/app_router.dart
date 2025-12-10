2f63cede8bea9b47f8ae26bf3b98bd2b84ec3921
mostafanagy <mostafanagy855@gmail.com>
2025-12-10 09:24:07 +0200

    home stracture

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -3,6 +3,7 @@ import 'package:crypto_x/features/auth/login/presentation/screens/login_screen.d
 import 'package:crypto_x/features/auth/login/presentation/screens/login_with_face_id_screen.dart';
 import 'package:crypto_x/features/auth/login/presentation/screens/login_with_finger_print_screen.dart';
 import 'package:crypto_x/features/auth/login/presentation/screens/verify_account_screen.dart';
+import 'package:crypto_x/features/home/presentation/ui/screens/home_screen.dart';
 import 'package:crypto_x/features/onboarding/presentation/screens/onboarding_screen.dart';
 import 'package:crypto_x/features/splash/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -29,6 +30,8 @@ class AppRouter {
 
       case Routes.verifyAccountScreen:
         return MaterialPageRoute(builder: (_) => const VerifyAccountScreen());
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
 
       default:
         return null;
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,7 +2,9 @@ class Routes {
   static const String splashScreen = '/splashScreen';
   static const String onBoardingScreen = '/onBoardingScreen';
   static const String loginScreen = '/loginScreen';
-  static const String loginWithFingerPrintScreen = '/loginWithFingerPrintScreen';
+  static const String loginWithFingerPrintScreen =
+      '/loginWithFingerPrintScreen';
   static const String loginWithFaceIdScreen = '/loginWithFaceIdScreen';
   static const String verifyAccountScreen = '/verifyAccountScreen';
+  static const String homeScreen = '/homeScreen';
 }
